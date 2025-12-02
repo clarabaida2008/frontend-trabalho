cf3ab80a18dbc98e8c0140805fe38401784c66df
Clara <csplicidobaida@gmail.com>
2025-12-02 08:25:57 -0400

    Edição do main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import App from './App.tsx'
 import Login from './componentes/login/login.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Admin from './componentes/admin/admin.tsx'
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CartaoPagamento from './componentes/pagamento/pagamento.tsx';
+
+const stripe = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY); // sua chave pública
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/finalizar-compra" element={<Elements stripe={stripe}><CartaoPagamento/></Elements>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
